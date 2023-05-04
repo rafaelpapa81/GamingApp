91fdc62e8a5111206c0f018340e3346a50de97af
Rafael Papa <rafaelpapa81@gmail.com>
2023-05-03 21:21:53 -0400

    gaming-app add svg images

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+//import Svg, { Path } from "react-native-svg"
+
+// icons
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
+// fonts
+import { useFonts } from 'expo-font';
+
+// colors
 import colors from './app/config/colors';
 
+// svg
+import GamingImg from './app/assets/gaming.svg';
+import ButterflyImg from './app/assets/floral-butterfly.svg';
+
 export default function App() {
+
+  //-----------------------
+  // load font
+  const [fontsLoaded] = useFonts({
+    'Roboto-MediumItalic': require('./app/assets/fonts/Roboto-MediumItalic.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  //-----------------------
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <Text style={styles.mainTitleText}>GAMEON</Text>
         {/*<StatusBar style="auto" />*/}
       </View>
+      <GamingImg width={300} height={300}/>
+      <ButterflyImg width={300} height={300} fill={'red'}/>
       <TouchableOpacity style={styles.letsGoButton}>
         <Text style={styles.arrowFwButton}>Let's Begin</Text>
         <MaterialIcons name="arrow-forward-ios" size={22} color='#fff' />
@@ -21,6 +46,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   arrowFwButton: {
+    fontFamily:'Roboto-MediumItalic',
     fontSize: 18,
     fontWeight: 'bold',
     color: colors.white,
@@ -38,6 +64,10 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  mainImg: {
+    width: 300,
+    height: 300,
   },
   mainTitleText: {
     fontSize: 30,
