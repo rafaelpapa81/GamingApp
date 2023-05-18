bffa6ee0381c1bd5fc1ba95dabba91a0b92f3172
Rafael Papa <rafaelpapa81@gmail.com>
2023-05-17 21:23:19 -0400

    gaming-app add navigation / re-structure the app

--- App.js
+++ App.js
@@ -1,88 +1,17 @@
 //import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-//import Svg, { Path } from "react-native-svg"
 
-// icons
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-
-// fonts
-import { useFonts } from 'expo-font';
-
-// colors
-import colors from './app/config/colors';
-
-// svg
-import GamingImg from './app/assets/gaming.svg';
-//import ButterflyImg from './app/assets/floral-butterfly.svg';
+import Home from './app/screens/HomeScreen';
+import Main from './app/screens/MainScreen';
 
 export default function App() {
-
-  //-----------------------
-  // load font
-  const [fontsLoaded] = useFonts({
-    'Roboto-MediumItalic': require('./app/assets/fonts/Roboto-MediumItalic.ttf'),
-  });
-
-  if (!fontsLoaded) {
-    return null;
-  }
-  //-----------------------
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.containerText}>
-        <Text style={styles.mainTitleText}>GAMEON</Text>
-        {/*<StatusBar style="auto" />*/}
-      </View>
-      <View style={styles.containerImg}>
-        <GamingImg width={300} height={300} style={styles.mainImg}/>
-      </View>
-      {/*<ButterflyImg width={300} height={300} fill={'red'}/>*/}
-      <TouchableOpacity style={styles.letsGoButton}>
-        <Text style={styles.arrowFwButton}>Let's Begin</Text>
-        <MaterialIcons name="arrow-forward-ios" size={22} color='#fff' />
-      </TouchableOpacity>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Main'>
+        <Stack.Screen name="Main" component={Main} options={{ title: 'Home' }} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  arrowFwButton: {
-    fontFamily:'Roboto-MediumItalic',
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: colors.white,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  containerImg: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  containerText: {
-    marginTop: 20,
-  },
-  letsGoButton: {
-    backgroundColor: colors.secondary,
-    padding: 20,
-    width:'90%',
-    borderRadius: 5,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 50,
-  },
-  mainImg: {
-    transform: [{rotate: '-15deg'}],
-  },
-  mainTitleText: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: colors.primary,
-  },
-});
---
+++ app/screens/MainScreen.js
@@ -0,0 +1,88 @@
+//import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+//import Svg, { Path } from "react-native-svg"
+
+// icons
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+// fonts
+import { useFonts } from 'expo-font';
+
+// colors
+import colors from '../config/colors';
+
+// svg
+import GamingImg from '../assets/gaming.svg';
+//import ButterflyImg from './app/assets/floral-butterfly.svg';
+
+function Main() {
+
+    //-----------------------
+    // load font
+    const [fontsLoaded] = useFonts({
+        'Roboto-MediumItalic': require('../assets/fonts/Roboto-MediumItalic.ttf'),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+    //-----------------------
+
+    return (
+    <SafeAreaView style={styles.container}>
+        <View style={styles.containerText}>
+            <Text style={styles.mainTitleText}>GAMEON</Text>
+            {/*<StatusBar style="auto" />*/}
+        </View>
+        <View style={styles.containerImg}>
+            <GamingImg width={300} height={300} style={styles.mainImg}/>
+        </View>
+        {/*<ButterflyImg width={300} height={300} fill={'red'}/>*/}
+        <TouchableOpacity style={styles.letsGoButton}>
+            <Text style={styles.arrowFwButton}>Let's Begin</Text>
+            <MaterialIcons name="arrow-forward-ios" size={22} color='#fff' />
+        </TouchableOpacity>
+    </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    arrowFwButton: {
+        fontFamily:'Roboto-MediumItalic',
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: colors.white,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    containerImg: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    containerText: {
+        marginTop: 20,
+    },
+    letsGoButton: {
+        backgroundColor: colors.secondary,
+        padding: 20,
+        width:'90%',
+        borderRadius: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 50,
+    },
+    mainImg: {
+        transform: [{rotate: '-15deg'}],
+    },
+    mainTitleText: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: colors.primary,
+    },
+});
