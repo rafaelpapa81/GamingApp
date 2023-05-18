1776600af47a203df252e028776a25935bbea8a1
Rafael Papa <rafaelpapa81@gmail.com>
2023-05-17 21:58:31 -0400

    gaming-app align text element to the center of screen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 function Home() {
     return (
-        <View>
+        <View style={styles.containerMain}>
             <Text>Home Screen</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    containerMain: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
 
 export default Home;
