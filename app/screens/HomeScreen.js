24d557df44da70b4a1771bf4de8636064835b174
Rafael Papa <rafaelpapa81@gmail.com>
2023-06-11 15:53:36 -0400

    gaming-app add user profile section

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,19 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, ScrollView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+// fonts
+import { useFonts } from 'expo-font';
 
 function Home() {
+
+    //-----------------------
+    // load font
+    const [fontsLoaded] = useFonts({
+        'Roboto-Medium': require('../assets/fonts/Roboto-Medium.ttf'),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+    //-----------------------
+
     return (
-        <View style={styles.containerMain}>
-            <Text>Home Screen</Text>
-        </View>
+        <SafeAreaView style={styles.containerMain}>
+            <ScrollView style={{padding:20}}>
+                <View style={styles.containerUserProfileSection}>
+                    <Text style={styles.userProfileText}>Hello Rafael Papa</Text>
+                    <ImageBackground 
+                        source={require('../assets/images/user-profile.jpg')}
+                        style={styles.userProfilePic}
+                        imageStyle={{borderRadius:25}}
+                    />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     containerMain: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#fff',
+    },
+    containerUserProfileSection: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+    },
+    userProfilePic:{
+        width: 35,
+        height: 35,
+    },
+    userProfileText:{
+        fontSize: 16,
+        fontFamily:'Roboto-Medium',
     },
 });
 
