f54be17dfcec1be1c7cfb19616035805e2eba7c4
Rafael Papa <rafaelpapa81@gmail.com>
2023-07-16 16:37:10 -0400

    gaming-app add screens to drawer-navigation

--- app/Navigation/AppStack.js
+++ app/Navigation/AppStack.js
@@ -11,14 +11,20 @@ const Drawer = createDrawerNavigator();
 
 // import screens
 import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import MessagesScreen from '../screens/MessagesScreen';
+import MomentsScreen from '../screens/MomentsScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
 
 const AppStack = () => {
     return (
         <Drawer.Navigator initialRouteName='Onboarding' screenOptions={{headerShown: false}}>
-            <Drawer.Screen 
-                name="Home" 
-                component={HomeScreen} 
-            />
+            <Drawer.Screen name="Home" component={HomeScreen} />
+            <Drawer.Screen name="Profile" component={ProfileScreen} />
+            <Drawer.Screen name="Messages" component={MessagesScreen} />
+            <Drawer.Screen name="Moments" component={MomentsScreen} />
+            <Drawer.Screen name="Settings" component={SettingsScreen} />
         </Drawer.Navigator>
     )
 }
