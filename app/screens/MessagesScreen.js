916b322cdcbb5ebcfe4b4e1f19d977532a833307
Rafael Papa <rafaelpapa81@gmail.com>
2023-07-16 16:20:44 -0400

    gaming-app add messages-screen

---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MessagesScreen = () => {
+    return (
+        <View style={styles.mainContainer}>
+            <Text>MessagesScreen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    mainContainer:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
+
+export default MessagesScreen;
