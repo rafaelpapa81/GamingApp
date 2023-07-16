22dac0959b02a5f6e3b77c940b0db1a2c09e92cb
Rafael Papa <rafaelpapa81@gmail.com>
2023-07-16 16:21:58 -0400

    gaming-app add moments-screen

---
+++ app/screens/MomentsScreen.js
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MomentsScreen = () => {
+    return (
+        <View style={styles.mainContainer}>
+            <Text>MomentsScreen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    mainContainer:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
+
+export default MomentsScreen;
