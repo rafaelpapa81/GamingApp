bc293cc8299be60f959edba5997cf59f8e8d8ce1
Rafael Papa <rafaelpapa81@gmail.com>
2023-07-16 17:16:42 -0400

    gaming-app add profile-pic and username to drawer

--- app/components/CustomDrawer.js
+++ app/components/CustomDrawer.js
@@ -1,17 +1,49 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, Image, StyleSheet } from 'react-native'
 import React from 'react'
 
 import {DrawerContentScrollView, DrawerItemList} from '@react-navigation/drawer'
 
 const CustomDrawer = (props) => {
     return (
-        <DrawerContentScrollView {...props}>
-            <DrawerItemList {...props} />
+        <View style={styles.mainContainer}>
+            <DrawerContentScrollView 
+                {...props}
+                contentContainerStyle={{backgroundColor: '#8200d6'}}
+            >
+                <ImageBackground source={require('../assets/images/menu-bg.jpeg')} style={styles.imageBackground}>
+                    <Image source={require('../assets/images/user-profile.jpg')} style={styles.imageProfile}/>
+                    <Text style={styles.userName}>John Doe</Text>
+                </ImageBackground>
+                <DrawerItemList {...props} />
+            </DrawerContentScrollView>
+            <View>
+                <Text>Our Custom Text</Text>
+            </View>
 
-        
-
-        </DrawerContentScrollView>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    mainContainer:{
+        flex: 1,
+    },
+    imageBackground:{
+        padding: 20,
+    },
+    imageProfile:{
+        height: 80,
+        width: 80,
+        borderRadius: 40,
+        marginBottom:10,
+    },
+    userName:{
+        color: '#fff',
+        fontSize: 18,
+        fontFamily:'Roboto-Medium',
+    },
+
+})
+
 
 export default CustomDrawer
