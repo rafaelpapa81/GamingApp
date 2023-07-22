28aa0f4d86cb676dca1872890a8c1bd9190817af
Rafael Papa <rafaelpapa81@gmail.com>
2023-07-22 11:17:47 -0400

    gaming-app display badge over icon on bottom-tab navigator

--- app/Navigation/TabNavigator.js
+++ app/Navigation/TabNavigator.js
@@ -30,6 +30,7 @@ const TabNavigator = () => {
                 )
             }}/>
             <Tab.Screen name='Cart' component={CartScreen} options={{
+                tabBarBadge: 3,
                 tabBarIcon: ({color, size}) => (
                     <FeatherIcon name='shopping-bag' color={color} size={size} />
                 )
