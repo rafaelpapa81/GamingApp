e1f5f409c9e7eb138e26de16177df9bbbf44b828
Rafael Papa <rafaelpapa81@gmail.com>
2023-07-22 11:07:32 -0400

    gaming-app add home-icon to bottom-tab navigator

--- app/Navigation/TabNavigator.js
+++ app/Navigation/TabNavigator.js
@@ -7,14 +7,25 @@ import HomeScreen from '../screens/HomeScreen';
 import CartScreen from '../screens/CartScreen';
 import FavoriteScreen from '../screens/FavoriteScreen';
 
+// icons
+import IoniconsIcon from 'react-native-vector-icons/Ionicons';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
 
 // create bottom tab navigator
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name='Home2' component={HomeScreen} />
+        <Tab.Navigator screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+        }}>
+            <Tab.Screen name='Home2' component={HomeScreen} options={{
+                tabBarIcon: ({color, size}) => (
+                    <IoniconsIcon name='home-outline' color={color} size={size} />
+                )
+            }}/>
             <Tab.Screen name='Cart' component={CartScreen} />
             <Tab.Screen name='Favorite' component={FavoriteScreen} />
         </Tab.Navigator>
