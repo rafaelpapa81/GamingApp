2eee9e7eca2d160466c0cac51dc145aa21d11a45
Rafael Papa <rafaelpapa81@gmail.com>
2023-07-26 21:14:10 -0400

    gaming-app authentication: provide string as key

--- app/context/AuthContext.js
+++ app/context/AuthContext.js
@@ -1,11 +1,14 @@
-import React, {createContext} from 'react';
+import React, {createContext, useState} from 'react';
 
 export const AuthContext = createContext();
 
 // pass any value to any screen of the app
 export const AuthProvider = ({children}) => {
+
+    const [test, setTest] = useState('Test Value');
+
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={{test}}>
             {children}
         </AuthContext.Provider>
     );
